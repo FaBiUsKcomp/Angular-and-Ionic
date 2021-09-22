bb5817c7f09d4c751d111ad6482da012d83bef56
FaBiUsKcomp <fabio@zeester.com.br>
2021-09-22 10:51:47 -0300

    :hammer: Adiciona suporte para o Toust.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private toastController: ToastController) {}
+
+  onCalculate = () => {
+    console.log("Clicou...")
+  }
+
+  showMessage = async () => {
+    const toast = await this.toastController.create({
+      message: "Clicou",
+      duration: 3000
+    })
+
+    toast.present()
+  }
 
 }
